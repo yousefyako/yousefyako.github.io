791522eb961452b7edea6c92a7e458f52e073019
yousefyako <yakoyousef@gmail.com>
2025-01-25 20:57:53 +0100

    Fully redesigned the website

---
+++ app.js
@@ -0,0 +1,22 @@
+const hamburgerMenu = document.querySelector("#hamburger");
+const hamburgerLine = document.querySelectorAll(".hamburgerLine");
+const menu = document.querySelector("#menu");
+const hLinks = document.querySelectorAll("#hLink");
+const body = document.querySelector("body");
+
+hamburgerMenu.addEventListener("click", () => {
+	closeMenu();
+});
+
+hLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		closeMenu();
+	});
+});
+
+const closeMenu = () => {
+	menu.classList.toggle("hidden");
+	hamburgerLine.forEach((line) => {
+		line.classList.toggle("bg-white");
+	});
+};
